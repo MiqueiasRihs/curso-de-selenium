4bec6393187f1beb77424842cdff66f00623290a
miqueias_obrazul <miqueias@obrazul.com.br>
2020-11-30 19:21:37 -0300

    css-selectors

--- aula-05/aula_05_01.py
+++ aula-05/aula_05_01.py
@@ -4,4 +4,9 @@ firefox = Firefox()
 
 firefox.get('http://selenium.dunossauro.live/aula_05_a.html')
 
-div_1 = firefox.find_element_by_id('lisp')
+div_py = firefox.find_element_by_id('python')
+div_hk = firefox.find_element_by_id('haskell')
+
+print(div_hk.text)
+
+firefox.quit()
---
+++ aula-06/aula_06_02.py
@@ -0,0 +1,27 @@
+from selenium.webdriver import Firefox
+
+b = Firefox()
+url = 'http://selenium.dunossauro.live/aula_06_a.html'
+
+b.get(url)
+
+# ===== Usando a atributo 'type' [att=valor] =====
+tag_type_nome = b.find_element_by_css_selector('[type="text"]')
+tag_type_senha = b.find_element_by_css_selector('[type="password"]')
+tag_type_btn = b.find_element_by_css_selector('[type="submit"]')
+
+# ===== Usando a atributo 'name' [att=valor] =====
+tag_name_nome = b.find_element_by_css_selector('[name="nome"]')
+tag_name_senha = b.find_element_by_css_selector('[name="senha"]')
+tag_name_btn = b.find_element_by_css_selector('[name="l0c0"]')
+
+# ===== Usando a atributo [att*=valor] =====
+nome = b.find_element_by_css_selector('[name*="ome"]')
+senha = b.find_element_by_css_selector('[name*="nha"]')
+btn = b.find_element_by_css_selector('[name*="l0"]')
+
+nome.send_keys('Miqueias')
+senha.send_keys('123')
+btn.click()
+
+builtin_function_or_method
